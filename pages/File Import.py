4c41ec278690be455a331f6717ccb553fab0651f
Konner Horton <konnerhorton@gmail.com>
2023-12-03 10:45:09 -0500

    Remove `data` from gitignore

--- pages/File Import.py
+++ pages/File Import.py
@@ -11,11 +11,9 @@ import streamlit as st
 from utilities.ags_names import AGS_VERBOSE_MAP
 
 
-input_ags = "data/East West Rail BGS Pre October 2018 upload (partial).ags"
+input_ags = "../data/East West Rail BGS Pre October 2018 upload (partial).ags"
 
-tables, headings = AGS4.AGS4_to_dataframe(
-    "../data/East West Rail BGS Pre October 2018 upload (partial).ags"
-)
+tables, headings = AGS4.AGS4_to_dataframe(input_ags)
 
 table_dict = {k: v for k, v in AGS_VERBOSE_MAP.items() if k in headings.keys()}
 table_list_text = "\n".join([f"- {k} - {v}" for k, v in table_dict.items()])
