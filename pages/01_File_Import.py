446beeb9528dff817688aec9ba03bf1bd353d095
Konner Horton <konnerhorton@gmail.com>
2023-12-21 08:55:06 -0500

    Update to check tempfile

--- pages/01_File_Import.py
+++ pages/01_File_Import.py
@@ -1,4 +1,6 @@
 # Import standard libraries
+import io
+import os
 import tempfile
 
 
@@ -12,30 +14,10 @@ import streamlit as st
 from utilities.ags_utils import ags_to_dfs
 from utilities.ags_utils import AGS_VERBOSE_MAP
 
+
 # Identify example file
-example_ags = "data/East West Rail BGS Pre October 2018 upload (partial).ags"
-file_path = example_ags
-
-st.header("Upload and validate your file")
-st.markdown(
-    """Use the sidebar (to the left) to upload your `.ags` file. The `AGS4.check_file()`
-    function will validate the file. Output from `check_file()` will be displayed below
-    as tables under their respective rule labels. Metadata for the file is shown last.
-    If the only table visible is the metadata table, then the file is `.ags` compatible."""
-)
-st.markdown(
-    """  
-    It may be the case that `check_file()` cannot be run on the uploaded file. In this
-    case an error message will appear below without and output tables."""
-)
-st.markdown(
-    """For a complete list of rules, refer to the documentation available at the
-    [AGS website](https://www.ags.org.uk/data-format/ags4-data-format/ags-4-1/)"""
-)
-st.markdown(
-    """Once your file is uploaded and validated, use the sidebar to navigate to other pages to explore your data."""
-)
-
+EXAMPLE_AGS_PATH = "data/East West Rail BGS Pre October 2018 upload (partial).ags"
+AGS_VALIDATION_RESULTS = None
 # Prompt user for input file
 with st.sidebar:
     ags_input = st.file_uploader(
@@ -43,21 +25,76 @@ with st.sidebar:
         type=".ags",
     )
 
-
 if ags_input:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+    with tempfile.NamedTemporaryFile(
+        delete=False, suffix=os.path.splitext(ags_input.name)[1]
+    ) as tmp_file:
         tmp_file.write(ags_input.getvalue())
-        file_path = tmp_file.name
+        PROJECT_AGS_PATH = tmp_file.name
+else:
+    PROJECT_AGS_PATH = EXAMPLE_AGS_PATH
 
 try:
-    results = AGS4.check_file(file_path)
-    st.session_state.ags_data = ags_to_dfs(file_path)
-    for k in results.keys():
-        st.write(k)
-        st.dataframe(results[k], use_container_width=True)
-
+    AGS_VALIDATION_RESULTS = AGS4.check_file(PROJECT_AGS_PATH)
 except:
     st.write(
         "Sorry, looks like this is not `.ags` compatible. Please check the contents and try again."
     )
-# st.write(type(ags_to_dfs(file_path)))
+
+if AGS_VALIDATION_RESULTS:
+    for k in AGS_VALIDATION_RESULTS.keys():
+        st.write(k)
+        st.dataframe(AGS_VALIDATION_RESULTS[k], use_container_width=True)
+    st.session_state.ags_data = ags_to_dfs(PROJECT_AGS_PATH)
+
+# PROJECT_AGS_RAW = create_file_like_object(EXAMPLE_AGS_PATH)
+
+# st.header("Upload and validate your file")
+# st.markdown(
+#     """Use the sidebar (to the left) to upload your `.ags` file. The `AGS4.check_file()`
+#     function will validate the file. Output from `check_file()` will be displayed below
+#     as tables under their respective rule labels. Metadata for the file is shown last.
+#     If the only table visible is the metadata table, then the file is `.ags` compatible."""
+# )
+# st.markdown(
+#     """
+#     It may be the case that `check_file()` cannot be run on the uploaded file. In this
+#     case an error message will appear below without and output tables."""
+# )
+# st.markdown(
+#     """For a complete list of rules, refer to the documentation available at the
+#     [AGS website](https://www.ags.org.uk/data-format/ags4-data-format/ags-4-1/)"""
+# )
+# st.markdown(
+#     """Once your file is uploaded and validated, use the sidebar to navigate to other pages to explore your data."""
+# )
+
+
+# load_ags()
+
+
+# def validate_ags():
+#     global AGS_VALIDATION_RESULTS
+#     try:
+#         AGS_VALIDATION_RESULTS = AGS4.check_file(PROJECT_AGS_PATH)
+#         # st.session_state.ags_data = ags_to_dfs(PROJECT_AGS_PATH)
+#         # st.write(AGS4.AGS4_to_dataframe(PROJECT_AGS_PATH))
+#         st.write(PROJECT_AGS_PATH)
+
+#     except:
+#         st.write(
+#             "Sorry, looks like this is not `.ags` compatible. Please check the contents and try again."
+#         )
+
+
+# def display_ags_results():
+#     try:
+#         for k in AGS_VALIDATION_RESULTS.keys():
+#             st.write(k)
+#             st.dataframe(AGS_VALIDATION_RESULTS[k], use_container_width=True)
+#     except:
+#         st.write("no dice")
+
+
+# validate_ags()
+# display_ags_results()
--- pages/02_Location_Map.py
+++ pages/02_Location_Map.py
@@ -9,22 +9,23 @@ from utilities.ags_utils import ags_to_dfs
 from utilities.ags_utils import AGS_VERBOSE_MAP
 
 st.title("Location Map")
-
+st.session_state.ags_data
 st.markdown(
     """Here, you can use the sidebar (to the left) to provide the Coordinate Reference System (CRS) to use for your project. The CRS you select should be one of those available at [epsg.io](https://epsg.io/). If you're project can not be represented by a standard value from [epsg.io](https://epsg.io/), you will have to use the closest approximation (and don't put too much stock in the precision on the map). This could be the case for Canada and Australia, where the use of grid shift files is common."""
 )
 
 crs_from = None
+crs_to = "EPSG:4326"
 df = None
 transformer = None
-crs_to = "EPSG:4326"
-crs_from = "EPSG:27700"
 
 with st.sidebar:
     st.markdown(
         "Identify the Coordinate Reference System (CRS) to use for your `.ags.` file. Use ONLY the numbers listed at [epsg.io](https://epsg.io/). For example, the sample data set uses EPSG:27700, so you would input `27700` in the space below."
     )
     crs_from = st.text_input(label="Input your CRS")
+    if crs_from:
+        st.session_state.crs_from = crs_from
 
 
 try:
@@ -35,11 +36,13 @@ except:
     )
 
 try:
-    transformer = Transformer.from_crs(crs_from, crs_to, always_xy=True)
+    transformer = Transformer.from_crs(
+        st.session_state.crs_from, crs_to, always_xy=True
+    )
 except:
     st.write("Please input a valid EDPS")
 
-if df is not None and not df.empty and transformer and crs_from:
+if df is not None and not df.empty and transformer and st.session_state.crs_from:
     for c in ["LOCA_NATE", "LOCA_NATN"]:
         df[c] = pd.to_numeric(df[c], errors="coerce")
 
